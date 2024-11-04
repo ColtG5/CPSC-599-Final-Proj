d32e3847ad9492e3d0475a43a482d958a74e9ae0
ColtG5 <colton.gowans@ucalgary.ca>
2024-11-03 18:07:08 -0700

    small init

---
+++ src/extras/stub.s
@@ -0,0 +1,8 @@
+; BASIC stub!!!
+
+; defined constant: word constant, byte constant, etc
+    dc.w nextstmt
+    dc.w 42
+    dc.b $9e, "4109", 0
+nextstmt
+    dc.w 257
---
+++ src/extras/util.s
@@ -0,0 +1,18 @@
+f_set_color_mem_black:
+    ldx #0
+_color_stuff_1:                      ; these next 15ish lines set all of color mem to black, so we can see the title screen
+    lda #0                          ; foreground black
+    sta $9600,x
+    inx
+    txa
+    cmp #00
+    bne _color_stuff_1
+
+_color_stuff_2:
+    lda #0
+    sta $9700,x
+    inx
+    txa
+    cmp #00
+    bne _color_stuff_2
+    jsr
