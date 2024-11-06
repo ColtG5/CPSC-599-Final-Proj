deb885d19cdd35cbdbf2e782e1ff3574b1621d72
Famington <87153249+Famington@users.noreply.github.com>
2024-11-06 04:45:53 -0700

    actually working :) just doesnt erase

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
 ALL = game.prg
 START = game.prg
-DASM = dasm
-REMOTE_SERVER = cpsc
+DASM = /c/Users/fam20/Desktop/CPSC-599-A1/Fambospecficfoldernotforgit/dasm.exe
+REMOTE_SERVER = fam.ghaly@cslinux.ucalgary.ca
 REMOTE_DIR = ~/www/
 
 all: $(ALL)
 
-game.prg: ./src/main.s
+
+music.prg: ./src/titlescreen_music.s ./src/extras/stub.s
+	$(DASM) $< -o$@ -l$(<:.s=.lst)
+
+game.prg: ./src/main.s ./src/extras/stub.s
 	$(DASM) $< -o$@ -l$(<:.s=.lst)
 
 clean:
@@ -17,4 +21,4 @@ upload: all
 	ssh $(REMOTE_SERVER) "cd $(REMOTE_DIR); chmod 644 $(ALL)"
 
 start:
-	start "https://cspages.ucalgary.ca/~aycock/599.82/vic20/?file=https://cspages.ucalgary.ca/~colton.gowans/$(START)"
+	start "https://cspages.ucalgary.ca/~aycock/599.82/vic20/?file=https://cspages.ucalgary.ca/~fam.ghaly/$(START)"
--- src/main.s
+++ src/main.s
@@ -1,10 +1,13 @@
     processor 6502
 
 CUSTOM_CHAR_MEM = $1c00                         ; custom char table start
-SCREEN_MEM = $1e00                              ; screen mem start!
+SCREEN_MEM = $1e00                              ; screen mem start
 CHARSET_POINTER = $9005                         ; custom char table vic chip mem address place
 GETIN = $ffe4
+PLOT = $fff0
+CHROUT = $FFD2
 
+; Zero-page variable declarations
 DATA_ADDR_LOW = $00
 DATA_ADDR_HIGH = $01
 LOAD_ADDR_LOW = $02
@@ -17,79 +20,162 @@ what_level_tracker = $07
 level_data_addr_low = $08
 level_data_addr_high = $09
 
+cursor_x = $0A                                  ; Cursor X position
+cursor_y = $0B                                  ; Cursor Y position
+portal_x = $0C                                  ; Portal X position
+portal_y = $0D                                  ; Portal Y position
+portal_placed = $0E                             ; 1 if portal is placed, 0 if not
+
     org $1001, 0
     include "./src/extras/stub.s"
 
     lda #255
     sta CHARSET_POINTER
 
-    ; initializing stuff
+    ; Initialize variables and start titlescreen
     lda #1
     sta what_level_tracker
-
     jsr f_set_color_mem_black
-
     jsr f_draw_titlescreen
 
 starting_loop:
     jsr GETIN
     cmp #0
-    beq play_music_and_wait_input     ; Wait if no input detected
+    beq play_music_and_wait_input               ; Wait if no input detected
 
     jsr f_clear_screen
-    jsr f_draw_level                            ; on any input, draw the first level, and start the game
+    jsr f_draw_level                            ; Draw first level on input and start the game
+    lda #8                                      ; Initialize cursor and portal positions
+    sta cursor_x
+    lda #5
+    sta cursor_y
+    lda #8
+    sta portal_x
+    lda #8
+    sta portal_y
+    lda #1
+    sta portal_placed
     jmp game_loop
 
 play_music_and_wait_input:
-    jsr f_play_melody                  ; Play titlescreen melody
-    jmp starting_loop                 ; Return to input loop
+    jsr f_play_melody                           ; Play titlescreen melody
+    jmp starting_loop                           ; Return to input loop
 
+; Main Game Loop
 game_loop:
+    jsr f_plot_portal                           ; Draw portal at current position
 
+    jsr GETIN                                   ; Get player input
+    cmp #0
+    beq game_loop                               ; Continue loop if no input
 
+    sta $04                                     ; Store input in temporary register
+    jsr f_handle_input                          ; Handle input
+    jmp game_loop                               ; Repeat loop
 
-
-    ; for testing purposes: if spacebar is hit, advance to next level
-    jsr GETIN
-    cmp #$20                    ; code for spacebar
-    bne game_loop
-
-    ; move to next level! if trying to move from level 3 to level 4, "restart" the game by going back to starting loop
-    lda what_level_tracker
-    cmp #3
-    bne dont_start_over
-    lda #1
-    sta what_level_tracker
-    jsr f_draw_titlescreen
-    jmp starting_loop
-
-dont_start_over:
-    inc what_level_tracker
-    jsr f_clear_screen
-    jsr f_draw_level
-    jmp game_loop
-    
+; Input Handling Subroutine
+f_handle_input:
+    lda $04
+    cmp #87                                     ; W key for up
+    beq f_move_up
+    cmp #65                                     ; A key for left
+    beq f_move_left
+    cmp #83                                     ; S key for down
+    beq f_move_down
+    cmp #68                                     ; D key for right
+    beq f_move_right
+    cmp #69                                     ; E key to pick/place portal
+    beq f_toggle_portal
     rts
 
+; Movement Functions
+f_move_up:
+    dec cursor_y
+    jsr f_draw_cursor
+    rts
 
+f_move_left:
+    dec cursor_x
+    jsr f_draw_cursor
+    rts
 
-    include "./src/extras/util.s"               ; util funcs
-    include "./src/compression/rle_decode.s"    ; rle decoder code
-    include "./src/titlescreen/titlescreen.s"   ; titlescreen
-    include "./src/levels/levels.s"             ; code to draw levles
-    include "./src/music/titlescreen_music.s"   ; Titlescreen music functions
+f_move_down:
+    inc cursor_y
+    jsr f_draw_cursor
+    rts
 
+f_move_right:
+    inc cursor_x
+    jsr f_draw_cursor
+    rts
 
-encoded_title_screen_data_start
+; Portal Placement Toggle Function
+f_toggle_portal:
+    lda portal_placed
+    beq f_place_portal                           ; If portal not placed, place it
+    jsr f_pickup_portal                          ; Otherwise, pick it up
+    rts
+
+f_place_portal:
+    lda cursor_x
+    sta portal_x
+    lda cursor_y
+    sta portal_y
+    lda #1
+    sta portal_placed
+    rts
+
+f_pickup_portal:
+    lda portal_x
+    cmp cursor_x
+    bne f_end_toggle
+    lda portal_y
+    cmp cursor_y
+    bne f_end_toggle
+    lda #0
+    sta portal_placed                            ; Set portal as not placed
+f_end_toggle:
+    rts
+
+; Drawing Functions
+f_draw_cursor:
+    ldx cursor_y
+    ldy cursor_x
+    clc
+    jsr PLOT
+    lda #64                                      ; Custom cursor character code
+    jsr CHROUT
+    rts
+
+f_plot_portal:
+    lda portal_placed
+    beq f_skip_plot_portal                       ; Skip if portal not placed
+    ldx portal_y
+    ldy portal_x
+    clc
+    jsr PLOT
+    lda #66                                      ; Custom portal character code
+    jsr CHROUT
+f_skip_plot_portal:
+    rts
+
+; Include supporting files as per conventions
+    include "./src/extras/util.s"                    ; Utility functions
+    include "./src/compression/rle_decode.s"         ; RLE decoder for titlescreen
+    include "./src/titlescreen/titlescreen.s"        ; Titlescreen logic
+    include "./src/levels/levels.s"                  ; Level drawing functions
+    include "./src/music/titlescreen_music.s"        ; Titlescreen music functions
+
+encoded_title_screen_data_start:
     incbin "./src/titlescreen/titlescreen-rle-encoded.bin"
 
-level_1_data_start
+level_1_data_start:
     incbin "./src/levels/level1.bin"
 
-level_2_data_start
+level_2_data_start:
     incbin "./src/levels/level2.bin"
 
-level_3_data_start
+level_3_data_start:
     incbin "./src/levels/level3.bin"
 
     org CUSTOM_CHAR_MEM
